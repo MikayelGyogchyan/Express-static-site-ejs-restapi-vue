da1b0a809ad0a23836a05f7dd33e4b1e74a780c5
Mikayel <gyogchyanmikayel@gmail.com>
2023-11-04 00:04:37 -0100

    Client-server app using vue.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import express from "express";
 import path from "path";
 import {requestTime, logger} from "./middlewares.js";
+import serverRoutes from "./routes/servers.js";
 
-const __dirname = path.resolve() // as we do not use 'require', but 'import', we create the '__dirname' apart
+const __dirname = path.resolve() 
 const PORT = process.env.PORT ?? 3000;
-const app = express(); // iniciamos nuestra app
+const app = express(); 
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve(__dirname, 'ejs'))
-// console.log(app.get('views')) // 33:00
 
 app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 app.use(requestTime)
 app.use(logger)
+
+app.use(serverRoutes)
 
 app.get('/', (req, res) => {
   res.render('index', {title: 'Main page', active: 'main'})
@@ -21,18 +25,6 @@ app.get('/', (req, res) => {
 app.get('/features', (req, res) => {
   res.render('features', {title: 'Features page', active: 'features'})
 })
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'static', 'index.html'));
-// });
-
-// app.get("/features", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'static', 'features.html'));
-// });
-
-// app.get("/download", (req, res) => {
-//   res.download(path.resolve(__dirname, 'static', 'index.html'));
-// });
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}...`);
